bef42cd6a548014746d3bad10d6454296f989445
Aditya prakash <adyprakash19@gmail.com>
2025-04-12 18:40:13 +0000

    dff

--- myApp.js
+++ myApp.js
@@ -1,17 +1,40 @@
 let express = require('express');
 let app = express();
 
-// Log "Hello World" to the console
 console.log("Hello World");
 
-// Create a route to respond to GET requests
-app.get('/', (req, res) => {
-  res.send('Hello, World from Express!');
-});
 
-// Start the server and make it listen on port 3000
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
 
-module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Hello World");
+
+
+
+
+
+ module.exports = app;
