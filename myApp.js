f9a66e1c044d7db34cd9255699cecd9c9e10e103
Aditya prakash <adyprakash19@gmail.com>
2025-04-12 18:45:29 +0000

    sdsd

--- myApp.js
+++ myApp.js
@@ -1,6 +1,17 @@
 let express = require('express');
 let app = express();
 
+// --> 7)  Mount the Logger middleware here
+// app.use(function middleware(req, res, next) {
+//   console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+//   next();
+// });
+
 console.log("Hello World");
+
+// Root route should respond with "Hello Express"
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
 
 module.exports = app;
